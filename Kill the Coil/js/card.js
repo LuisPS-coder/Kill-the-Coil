9db777f8e4cb9f4be935081125cd2c74d177e137
LuisPS-coder <lps_1992@hotmail.com>
2024-11-16 04:10:48 +0100

    Update card.js

--- Kill the Coil/js/card.js
+++ Kill the Coil/js/card.js
@@ -1,13 +1,13 @@
-export class Card {
+class Card {
     constructor(name, cost, description, image, effect) {
-        this.name = name;           // Nombre de la carta
-        this.cost = cost;           // Costo en energía para jugar la carta
-        this.description = description; // Descripción de la carta
-        this.image = image;         // Ruta de imagen o nombre de archivo de imagen
-        this.effect = effect;       // Función de efecto de la carta (curar, hacer daño, etc.)
+        this.name = name;
+        this.cost = cost;
+        this.description = description;
+        this.image = image;
+        this.effect = effect;
     }
 
     use(target) {
-        this.effect(target);  // Aplica el efecto de la carta al objetivo
+        this.effect(target);
     }
 }
