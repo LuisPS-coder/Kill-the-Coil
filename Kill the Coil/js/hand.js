a1063579a317974c24830ce26dcfcecd8f6402a2
LuisPS-coder <lps_1992@hotmail.com>
2024-11-16 04:11:45 +0100

    ajustadas funciones de control de mano

--- Kill the Coil/js/hand.js
+++ Kill the Coil/js/hand.js
@@ -1,23 +1,38 @@
-export class Hand {
-    constructor() {
-        this.cards = [];
-        this.tamañoMax = 5;
+class Hand {
+    constructor(player) {
         this.player = player;
+        this.cardsInHand = [];
+        this.handContainer = document.getElementById('playerHand');
     }
 
-    robarCarta(numero) {
-        for (let i= 0; i < numero; i++) {
-            if (this.player.deck.length === 0)
-                this.player.barajarMazo();
-
-            if (this.player.deck.length > 0) {
-                const nuevaCarta = this.player.deck.pop();
-                this.cards.push (nuevaCarta)
-            }
-            else {
-                console.log ("No se pueden robar cartas");
-                break;
-            }
+    addCard(card) {
+        if (this.cardsInHand.length < 5) {
+            this.cardsInHand.push(card);
+            this.renderHand();
         }
+    }
+
+    renderHand() {
+        this.handContainer.innerHTML = '';
+
+        this.cardsInHand.forEach((card, index) => {
+            const cardElement = document.createElement('div');
+            cardElement.classList.add('card');
+            cardElement.innerText = `${card.name}\nCost: ${card.cost}`;
+
+            cardElement.onclick = () => {
+                if (this.player.useEnergy(card.cost)) {
+                    this.playCard(card, index);
+                }
+            };
+
+            this.handContainer.appendChild(cardElement);
+        });
+    }
+
+    playCard(card, index) {
+        this.cardsInHand.splice(index, 1);
+        this.player.quitarCarta(card);
+        this.renderHand();
     }
 }
